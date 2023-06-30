4a57176555662bd5e74e6839b5a89f89e1c2b95a
HajerBchn <hr.bouchenine@gmail.com>
2023-06-30 22:47:37 +0100

    Add meta data to index.html

--- src/App.js
+++ src/App.js
@@ -22,7 +22,6 @@ function App() {
           <meta property="og:description" content="Impact of code is a one-week-long dev event that consists of daily coding challenges wrapped up in a story scenario under the theme of an apocalyptic world (prequel to last year’s story) in which we follow the steps of a character as he solves his way to escape the end." />
           <meta name="description" content="Impact of code is a one-week-long dev event that consists of daily coding challenges wrapped up in a story scenario under the theme of an apocalyptic world (prequel to last year’s story) in which we follow the steps of a character as he solves his way to escape the end." />
           <meta property="og:image" content="https://www.dropbox.com/scl/fi/os0s4wurs2foe8oevyd6w/site-image.jpg?dl=0&rlkey=gtrpl017prp956gs0izcp3x9x" />
-
         </Helmet>
         <Navbar />
         <Hero />
