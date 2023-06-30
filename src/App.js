b457384aa5790a068e7cef71a750c1346dc06fad
HajerBchn <hr.bouchenine@gmail.com>
2023-06-30 23:13:57 +0100

    Remove Helmet

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Helmet, HelmetProvider } from 'react-helmet-async';
 import Navbar from './components/Navbar';
 import Hero from './sections/hero';
 import TheEvent from './sections/theEvent';
@@ -14,15 +13,7 @@ function App() {
   const helmetContext = {};
   return (
     <ParallaxProvider>
-    <HelmetProvider context={helmetContext}>
       <div className="App bg-black overflow-x-hidden">
-        <Helmet>
-          <title>Impact of Code</title>
-          <meta property="og:title" content="Impact of Code" />
-          <meta property="og:description" content="Impact of code is a one-week-long dev event that consists of daily coding challenges wrapped up in a story scenario under the theme of an apocalyptic world (prequel to last year’s story) in which we follow the steps of a character as he solves his way to escape the end." />
-          <meta name="description" content="Impact of code is a one-week-long dev event that consists of daily coding challenges wrapped up in a story scenario under the theme of an apocalyptic world (prequel to last year’s story) in which we follow the steps of a character as he solves his way to escape the end." />
-          <meta property="og:image" content="https://www.dropbox.com/scl/fi/os0s4wurs2foe8oevyd6w/site-image.jpg?dl=0&rlkey=gtrpl017prp956gs0izcp3x9x" />
-        </Helmet>
         <Navbar />
         <Hero />
         <TheEvent />
@@ -32,7 +23,6 @@ function App() {
         <Faq />
         <Footer />
       </div>
-      </HelmetProvider>
     </ParallaxProvider>
   );
 }
