e4884ba16cb8f001fcbba6227c15acd25e3d4e15
HajerBchn <hr.bouchenine@gmail.com>
2023-06-30 21:43:46 +0100

    Add Helmet component with meta data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import Navbar from './components/Navbar';
 import Hero from './sections/hero';
 import TheEvent from './sections/theEvent';
@@ -12,16 +13,22 @@ import { ParallaxProvider } from 'react-scroll-parallax';
 function App() {
   return (
     <ParallaxProvider>
-    <div className="App bg-black overflow-x-hidden">
-      <Navbar />
-      <Hero />
-      <TheEvent />
-      <TheChallenge />
-      <RulesAndConditions />
-      <Recording />
-      <Faq />
-      <Footer />
-    </div>
+      <div className="App bg-black overflow-x-hidden">
+        <Helmet>
+          <title>Impact of Code</title>
+          <meta property="og:title" content="Impact of Code" />
+          <meta property="og:description" content="Impact of code is a one-week-long dev event that consists of daily coding challenges wrapped up in a story scenario under the theme of an apocalyptic world (prequel to last year’s story) in which we follow the steps of a character as he solves his way to escape the end." />
+          <meta property="og:image" content="https://www.dropbox.com/scl/fi/os0s4wurs2foe8oevyd6w/site-image.jpg?dl=0&rlkey=gtrpl017prp956gs0izcp3x9x" />
+        </Helmet>
+        <Navbar />
+        <Hero />
+        <TheEvent />
+        <TheChallenge />
+        <RulesAndConditions />
+        <Recording />
+        <Faq />
+        <Footer />
+      </div>
     </ParallaxProvider>
   );
 }
